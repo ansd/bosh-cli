c80a8620b36f27941e80d5835b4b4836ffbfc972
David Ansari <david.ansari@sap.com>
2018-01-03 17:46:41 +0100

    Only output director error message when no such config exists
    [#150705441](https://www.pivotaltracker.com/story/show/150705441)

--- director/config_diff.go
+++ director/config_diff.go
@@ -3,6 +3,7 @@ package director
 import (
 	"encoding/json"
 	"net/http"
+	"strings"
 
 	bosherr "github.com/cloudfoundry/bosh-utils/errors"
 )
@@ -27,8 +28,16 @@ func (c Client) postConfigDiff(path string, manifest []byte, setHeaders func(*ht
 	respBody, response, err := c.clientRequest.RawPost(path, manifest, setHeaders)
 	if err != nil {
 		if response != nil && response.StatusCode == http.StatusNotFound {
-			// return empty diff, just for compatibility with directors which don't have the endpoint
-			return resp, nil
+			if strings.Contains(err.Error(), "\"code\": 440012") {
+				// config couldn't be found => return only the director error description
+				prefixRemoved := strings.TrimPrefix(err.Error(),
+					"Director responded with non-successful status code '404' response '{\"code\": 440012, \"description\": \"")
+				errorDescription := strings.TrimSuffix(prefixRemoved, "\"}'")
+				return resp, bosherr.Errorf(errorDescription)
+			} else {
+				// endpoint couldn't be found => return empty diff, just for compatibility with directors which don't have the endpoint
+				return resp, nil
+			}
 		} else {
 			return resp, bosherr.WrapErrorf(err, "Fetching diff result")
 		}
